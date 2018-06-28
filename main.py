412f0270ed47953cbb08c67b972d75974d22d598
aleckwhite <alexander.k.white@gmail.com>
2018-06-28 09:45:41 -0400

    centers golden braid and generates random color for the gradient

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 
 
 class EternalGoldenBraid(object):
@@ -34,16 +35,35 @@ class EternalGoldenBraid(object):
         my_turtle.end_fill()
         my_turtle.penup()
 
+    @staticmethod
+    def center_turtle_for_drawing(my_turtle):
+        my_turtle.hideturtle()
+        my_turtle.penup()
+        my_turtle.forward(70)
+        my_turtle.right(90)
+        my_turtle.forward(70)
+        my_turtle.left(90)
+
+    @staticmethod
+    def generate_random_color_gradient_list():
+        # todo get input color and convert to hex
+        # todo get hex stopping point programmatically
+        start_color = generate_random_hex_color_code()
+        color_int = int(start_color[1:], base=16)
+        color_int += 12800
+        end_color = '#' + str(hex(color_int))[2:]
+        print("start color: " + start_color)
+        print("end color: " + end_color)
+        # todo handle out of range
+        # todo make sure they are shades of same color
+        return linear_gradient(start_color, end_color, 3)
+
     def draw_eternal_golden_braid(self):
         window = turtle.Screen()
         window.bgcolor("black")  # background color
         tom = turtle.Turtle()
-        tom.hideturtle()
-        tom.penup()
-        tom.forward(70)
-        # todo get input color and convert to hex
-        # todo get hex stopping point programmatically
-        color_list = linear_gradient('#FFD700', '#FFA500', 3)
+        self.center_turtle_for_drawing(tom)
+        color_list = self.generate_random_color_gradient_list()
         for i in range(3):
             # determine color
             color = color_list['hex'][i]
@@ -53,11 +73,26 @@ class EternalGoldenBraid(object):
             tom.left(180)
         window.exitonclick()
 
+
+def generate_random_hex_color_code():
+    values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15']
+    hex_values = {'10': 'A', '11': 'B', '12': 'C', '13': 'D', '14': 'E', '15': 'F'}
+    color = "#"
+    for _ in range(6):
+        x = values[random.randint(0, len(values) - 1)]
+        if x in hex_values.keys():
+            x = hex_values[x]
+        color += x
+    return color
+
+
+
 def hex_to_RGB(hex):
     # https://bsou.io/posts/color-gradients-with-python
     ''' "#FFFFFF" -> [255,255,255] '''
     # Pass 16 to the integer function for change of base
     return [int(hex[i:i + 2], 16) for i in range(1, 6, 2)]
+
 
 def RGB_to_hex(RGB):
     # https://bsou.io/posts/color-gradients-with-python
@@ -67,6 +102,7 @@ def RGB_to_hex(RGB):
     return "#" + "".join(["0{0:x}".format(v) if v < 16 else
                           "{0:x}".format(v) for v in RGB])
 
+
 def color_dict(gradient):
     ''' Takes in a list of RGB sub-lists and returns dictionary of
       colors in RGB and hex form for use in a graphing function
@@ -75,6 +111,7 @@ def color_dict(gradient):
             "r": [RGB[0] for RGB in gradient],
             "g": [RGB[1] for RGB in gradient],
             "b": [RGB[2] for RGB in gradient]}
+
 
 def linear_gradient(start_hex, finish_hex="#FFFFFF", n=10):
     ''' returns a gradient list of (n) colors between
